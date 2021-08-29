45327e6c9240881f020da741bea69bdb6b8be9f4
Michael Kidby <68043023+babykittenz@users.noreply.github.com>
2021-08-29 15:55:05 -0400

    Update gulpfile.bable.js

--- gulpfile.bable.js
+++ gulpfile.bable.js
@@ -2,18 +2,47 @@ import gulp from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
+import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin'; //npm i gulp-imagemin@7.1.0
 
 
 
 
 const PRODUCTION = yargs.argv.prod;
 
+const paths = {
+  styles: {
+    src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+    dest: 'dist/assets/css'
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+    dest: 'dist/assets/images'
+  }
+}
+
 export const styles = () => {
 
   let sass = require('gulp-sass')(require('sass'));
+ 
+ 
 
-  return gulp.src('src/assets/scss/bundle.scss')
+  return gulp.src(paths.styles.src)
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/assets/css'));
+    .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(paths.styles.dest));
 }
+
+
+
+
+export const watch = () => {
+  gulp.watch( 'src/assets/scss/**/*.scss', styles);
+}
+
+
+
+// exports.default = hello;
